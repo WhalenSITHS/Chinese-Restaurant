fcd090f639d7dac8327e3287336064920ededb85
Michael Whalen <michael.whalen@sitechhs.com>
2019-05-26 12:19:08 -0400

    navigation

---
+++ app.js
@@ -0,0 +1,20 @@
+/* let images = [];
+let i =0;
+images [0] = 'img/atmo-1.jpg';
+images [1] = 'img/atmo-2.jpg';
+images [2] = 'img/atmo-3.jpg';
+
+/* function backgroundChange() {
+
+ var header = document.querySelector(".header").style.backgroundImage = "url 'images[0]'";
+}
+backgroundChange; */
+
+
+/* document.querySelector('.header').style.backgroundImage = " url('img/atmo-1.jpg')" */
+
+// document.querySelector('.header').style.backgroundImage = " url('images[0]')"
+
+function backgroundChange(){
+
+} */
